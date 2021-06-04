0f3cfff073be1dd7fa8c6766ada23469e49b9bac
Hoàng Bảo Vũ <baovuhbv@gmail.com>
2021-06-04 11:03:19 +0700

    Updated

--- components/ControlDevice/ControlDeviceTable.js
+++ components/ControlDevice/ControlDeviceTable.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Table, Tag, Radio, Space } from 'antd';
+import { Table, Tag, Radio, Space, Button, Tooltip } from 'antd';
+import { PlayCircleTwoTone, StopTwoTone, EditTwoTone, DeleteTwoTone } from '@ant-design/icons';
 
 export function ControlDeviceTable() {
 
@@ -9,50 +10,80 @@ export function ControlDeviceTable() {
     alert(key);
   }
 
+  const STATUS_CODE = {
+    0: 'OFF',
+    1: 'ON'
+  }
+
   const columns = [
     {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
+      title: 'STT',
+      dataIndex: 'index',
+      key: 'index',
       render: text => <a>{text}</a>,
+      sorter: {
+        compare: (a, b) => a.index - b.index,
+        multiple: 2,
+      },
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
+      title: 'Serial',
+      dataIndex: 'serial',
+      key: 'serial',
+      render: text => <a>{text}</a>,
+      sorter: {
+        compare: (a, b) => a.serial - b.serial,
+        multiple: 1,
+      },
     },
     {
       title: 'Status',
       key: 'status',
       dataIndex: 'status',
-      render: status => (
-        <span>
-          {status.map(tag => {
-            let color = tag.length > 5 ? 'geekblue' : 'green';
-            if (tag === 'loser') {
-              color = 'volcano';
-            }
-            return (
-              <Tag color={color} key={tag}>
-                {tag.toUpperCase()}
-              </Tag>
-            );
-          })}
-        </span>
-      ),
+      render: status => {
+        let color = 'error';
+
+        if (status === 'ON') {
+          color = 'green';
+        }
+        
+        return (
+          <span>
+            <Tag color={color} key={status}>
+              { status }
+            </Tag>
+          </span>
+        )
+      },
+      filters: [
+        {
+          text: 'OFF',
+          value: 'OFF',
+        },
+        {
+          text: 'ON',
+          value: 'ON',
+        },
+      ],
+      onFilter: (value, record) => record.status.includes(value),
     },
     {
-      title: 'Action',
+      title: 'Hành động',
       key: 'action',
       render: (text, record) => (
         <Space size="middle">
-          <a onClick={(e) => { onDelete(record.key, e); }}>Sửa {record.key}</a>
-          <a>Xóa</a>
+          <Tooltip title="Chạy">
+            <Button onClick={(e) => { onDelete(record.key, e); }} shape="circle" icon={<PlayCircleTwoTone twoToneColor="#1890ff" />} />
+          </Tooltip>
+          <Tooltip title="Dừng">
+            <Button onClick={(e) => { onDelete(record.key, e); }} shape="circle" icon={<StopTwoTone twoToneColor="#eb2f96" />} />
+          </Tooltip>
+          <Tooltip title="Sửa">
+            <Button onClick={(e) => { onDelete(record.key, e); }} shape="circle" icon={<EditTwoTone twoToneColor="#f1c40f" />} />
+          </Tooltip>
+          <Tooltip title="Xóa">
+            <Button onClick={(e) => { onDelete(record.key, e); }} shape="circle" icon={<DeleteTwoTone twoToneColor="#c0392b" />} />
+          </Tooltip>
         </Space>
       ),
     },
@@ -61,26 +92,28 @@ export function ControlDeviceTable() {
   const data = [
     {
       key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-      status: ['nice'],
+      serial: 'Serial 1',
+      status: 0,
     },
     {
       key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-      status: ['loser'],
+      serial: 'Serial 2',
+      status: 0,
     },
     {
       key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-      status: ['cool'],
+      serial: 'Serial 3',
+      status: 1,
     },
   ];
+
+  const page = 1;
+  const limit = 10;
+
+  data.forEach((element, index) => {
+    element['index'] = (page - 1) * limit + index + 1;
+    element['status'] = STATUS_CODE[element.status];
+  });
 
   return (
     <div>
--- layouts/Layout.js
+++ layouts/Layout.js
@@ -4,29 +4,26 @@ import 'antd/dist/antd.min.css';
 import { Layout as ALayout, Menu, Breadcrumb } from 'antd';
 const { Header, Content, Footer } = ALayout;
 
-
 export function Layout({ children }) {
   return (
     <ALayout>
       <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
         <div className="logo" />
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu.Item key="1">Điều khiển</Menu.Item>
+          <Menu.Item key="2">Thông tin</Menu.Item>
         </Menu>
       </Header>
       <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
         <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
+          <Breadcrumb.Item>Trang chủ</Breadcrumb.Item>
+          <Breadcrumb.Item>Điều khiển</Breadcrumb.Item>
         </Breadcrumb>
         <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
           { children }
         </div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>PKH Technologies &copy;2021</Footer>
+      <Footer style={{ textAlign: 'center' }}>&copy;2021 PKH Technologies</Footer>
     </ALayout>
   )
 }
